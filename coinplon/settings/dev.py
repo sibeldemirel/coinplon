be91fb2b14003df7898b1717e5844e1535e7f049
Sibel Demirel <sibeldmrl19@gmail.com>
2022-10-10 15:12:12 +0200

    coinplon wagtail

---
+++ coinplon/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-r5ow&k35y*$pezfq^-j36(@k_g1&(w0-^y&ev%1ubx8z!tksl="
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
